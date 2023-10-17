dfcc413e3dbc93098267e517f107a5b49c430f50
benjamin5252 <david.l@amaryllo.us>
2023-10-17 10:39:55 +0800

    <deploy>

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/react_encrypt_private/',
+  base: '/web_crypto_api_example/',
   plugins: [react()],
 })
